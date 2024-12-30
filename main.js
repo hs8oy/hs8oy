23575eb3339b53ed00d0f1a95dafd7510cb5c330
حـــســن <145502119+hs8oy@users.noreply.github.com>
2024-12-30 09:41:02 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+ScrollReveal().reveal('section', {
+    duration: 2000, // مدة الحركة
+    origin: 'bottom', // اتجاه الحركة
+    distance: '80px', // المسافة
+    delay: 100 // التأخير
+});
+
+ScrollReveal().reveal('.c', {
+    duration: 2000, // مدة الحركة
+    origin:"bottom",
+    distance: '10px', // المسافة
+    delay: 1000 // التأخير
+});
+
+ScrollReveal().reveal('.h', {
+    duration: 2000, // مدة الحركة
+    origin: 'bottom', // اتجاه الحركة
+    distance: '10px', // المسافة
+    delay: 1500 // التأخير
+});
+
+ScrollReveal().reveal('.j', {
+    duration: 2000, // مدة الحركة
+    origin: 'bottom', // اتجاه الحركة
+    distance: '10px', // المسافة
+    delay: 2000 // التأخير
+});
